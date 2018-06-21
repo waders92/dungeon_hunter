bc1232186593d4ec7ad2f301a2c00537caee742f
Wade Carlson <wcarlsondev@gmail.com>
2018-06-20 19:59:18 -0500

    can add records to mongo via the form

--- helper/app.js
+++ helper/app.js
@@ -1,6 +1,7 @@
 const db = require('../models');
 
 exports.createGame = function(req, res) {
+  console.log(req.body);
   db.PlayerScore.create(req.body)
   .then(function(newGame){
     res.status(201).json(newGame);
--- public/scripts/formCheck.js
+++ public/scripts/formCheck.js
@@ -1,6 +1,6 @@
 function saveGame() {
-  var player_name = document.gameForm.player.value;
-  var character_name = document.gameForm.character.value;
+  var player_name = document.gameForm.player_name.value.toString();
+  var character_name = document.gameForm.character_name.value.toString();
   var rounds = document.gameForm.rounds.value;
   document.gameForm.submit();
 }
