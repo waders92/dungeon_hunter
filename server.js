115fbd708c3188f4f737a023769bf1eb0e3ba5e1
Wade Carlson <wcarlsondev@gmail.com>
2018-06-23 11:25:18 -0500

    added handelbars templating

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const routes = require('./routes/app.js');
 const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars').create({ defaultLayout: 'main', extname: '.hbs' });
 
 
 app.use(express.static(__dirname + '/public'));
@@ -12,8 +13,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', routes);
 
 app.get('/', (req, res) => {
-  res.render('index.html');
+  res.render('index.hbs');
 });
+
+app.engine('hbs', exphbs.engine);
+app.set('view engine', 'hbs');
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
