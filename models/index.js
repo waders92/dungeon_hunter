998126044c761e57b5c6065de9cb5c346ac60b7d
Wade Carlson <wcarlsondev@gmail.com>
2018-07-03 20:51:45 -0500

    switching to process.env

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
-var keys = require('../keys.js');
-var dbUrl =  'mongodb://' + keys.NAME + ':' + keys.PASSWORD + '@ds123171.mlab.com:23171/dungeon-hunter';
+var dbUrl =  'mongodb://' + process.env.NAME + ':' + process.env.PASSWORD + '@ds123171.mlab.com:23171/dungeon-hunter';
 mongoose.set('debug', true);
 mongoose.connect(dbUrl);
 
